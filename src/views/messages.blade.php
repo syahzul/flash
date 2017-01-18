168c502b6303defd289454f3d10d86beda9ca8e1
Rouven Alexander Rieker <therouv@googlemail.com>
2017-01-18 16:28:12 +0100

    [TASK] Show close button for important messages

--- src/views/messages.blade.php
+++ src/views/messages.blade.php
@@ -10,7 +10,7 @@
                     alert-{{ $message->get('level') }}
                     {{ $message->has('important') ? 'alert-important' : '' }}"
         >
-            @if(!$message->has('important'))
+            @if($message->has('important'))
                 <button type="button"
                         class="close"
                         data-dismiss="alert"
